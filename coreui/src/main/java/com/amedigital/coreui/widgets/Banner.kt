b8633e258fb999e18cb1376e2b8c5e259668c4fe
Alexnaldo Santos <3rd.alexnaldo.santos@amedigital.com>
2022-04-27 11:00:26 -0300

    done banner component

--- coreui/src/main/java/com/amedigital/coreui/widgets/Banner.kt
+++ coreui/src/main/java/com/amedigital/coreui/widgets/Banner.kt
@@ -10,14 +10,21 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
+
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun Banner(images: List<String>, onImageClick: (image: String) -> Unit) {
 
@@ -25,16 +32,30 @@ fun Banner(images: List<String>, onImageClick: (image: String) -> Unit) {
         return if (old == current) 1f else 0.6f
     }
 
-    val (currentImage, setCurrentImage) = remember { mutableStateOf(images[0]) }
+    fun setCurrentImage(image: String, state: PagerState, scope: CoroutineScope){
+        val page = images.indexOf(image)
+        scope.launch {
+            state.animateScrollToPage(page)
+        }
+    }
+
+    if (images.isEmpty()) return
+
+    val pagerState = rememberPagerState()
+    val coroutineScope = rememberCoroutineScope()
 
     Box(
+        modifier = Modifier.height(98.dp),
         contentAlignment = Alignment.BottomCenter
     ) {
-        AsyncImage(
-            modifier = Modifier.clickable { onImageClick(images[0]) },
-            model = images[0],
-            contentDescription = "Banner Image"
-        )
+        HorizontalPager(count = images.count(), state = pagerState) {
+            AsyncImage(
+                modifier = Modifier
+                    .clickable { onImageClick(images[pagerState.currentPage]) },
+                model = images[pagerState.currentPage],
+                contentDescription = "Banner Image"
+            )
+        }
         Row(
             modifier = Modifier.padding(vertical = 4.dp)
         ) {
@@ -43,9 +64,14 @@ fun Banner(images: List<String>, onImageClick: (image: String) -> Unit) {
                     modifier = Modifier
                         .height(8.dp)
                         .width(8.dp)
-                        .clickable { setCurrentImage(image) },
+                        .clickable { setCurrentImage(image, pagerState, coroutineScope) },
                     shape = CircleShape,
-                    backgroundColor = Color.White.copy(alpha = getAlpha(image, currentImage))
+                    backgroundColor = Color.White.copy(
+                        alpha = getAlpha(
+                            image,
+                            images[pagerState.currentPage]
+                        )
+                    )
                 ) {}
                 Spacer(modifier = Modifier.width(4.dp))
             }
